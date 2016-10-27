4bd4136531c0f80e108c9a93057b09cc5292e284
Justin <justindado@yahoo.com>
2016-10-27 17:00:40 +1300

    updated server

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
-import express from 'express';
-import { apolloServer } from 'graphql-tools';
-import Schema from './data/schema';
-import Mocks from './data/mocks';
+import express from 'express'
+import Schema from './data/schema'
+import Resolvers from './data/resolvers'
+// import Mocks from './data/mocks'
+import bodyParser from 'body-parser'
 
-const GRAPHQL_PORT = 8080;
+import { apolloExpress, graphiqlExpress } from 'apollo-server'
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools'
 
-const graphQLServer = express();
-graphQLServer.use('/graphql', apolloServer({
-  graphiql: true,
-  pretty: true,
-  schema: Schema,
-  mocks: Mocks,
-}));
+const GRAPHQL_PORT = 8080
+
+const graphQLServer = express()
+
+const executableSchema = makeExecutableSchema({
+  typeDefs: Schema,
+  resolvers: Resolvers,
+  allowUndefinedInResolve: false,
+  printErrors: true,
+})
+
+graphQLServer.use('/graphql', bodyParser.json(), apolloExpress({
+  schema: executableSchema,
+  context: {},
+}))
+graphQLServer.use('graphiql', graphiqlExpress({
+  endpointURL: '/graphql'
+}))
+
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
   `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}/graphql`
-));
+))
