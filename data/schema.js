57537deb47e89b60f0a7aa695ba21b53902a5be2
Justin <justindado@yahoo.com>
2016-10-27 17:00:24 +1300

    updated schema

--- data/schema.js
+++ data/schema.js
@@ -1,6 +1,22 @@
 const typeDefinitions = `
 type Query {
-  testString: String
+  author(firstName: String, lastName: String): Author
+  getFortuneCookie: String
+}
+
+type Author {
+  _id: Int
+  firstName: String
+  lastName: String
+  posts: [Post]
+}
+
+type Post {
+  _id: Int
+  title: String
+  content: String
+  views: Int
+  author: Author
 }
 
 schema {
