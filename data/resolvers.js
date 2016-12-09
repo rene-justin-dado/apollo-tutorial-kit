d791c4aa47cbd646942a275fef6b255f40b29f8b
Justin <justindado@yahoo.com>
2016-12-09 17:23:50 +1300

    using mock data with connected sqlite

--- data/resolvers.js
+++ data/resolvers.js
@@ -1,20 +1,18 @@
+import { Author } from './connectors'
 const resolvers = {
   Query: {
-    author(root, args) {
-      return { id: 1, firstName: 'Hello', lastName: 'World' }
+    author(_, args) {
+      return Author.find({ where: args })
     }
   },
   Author: {
     posts(author) {
-      return [
-        { id: 1, title: 'A post', text: 'Some text', views: 2 },
-        { id: 2, title: 'Another post', text: 'Some other text', views: 200 }
-      ]
+      return author.getPosts()
     }
   },
   Post: {
     author(post) {
-      return { id: 1, firstName: 'Hello', lastName: 'World' }
+      return post.getAuthor
     }
   }
 }
