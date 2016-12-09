d7ea87fcea185628b105983971f8a43c39407f9b
Justin <justindado@yahoo.com>
2016-12-09 17:05:56 +1300

    added resolvers for real data

--- data/mocks.js
+++ data/mocks.js
@@ -1,5 +1,14 @@
+import casual from 'casual'
+
 const mocks = {
   String: () => 'It works!',
+  Query: () => ({
+    author: (root, args) => {
+      return { firstName: args.firstName, lastName: args.lastName }
+    }
+  }),
+  Author: () => ({ firstName: () => casual.firstName, lastName: () => casual.lastName }),
+  Post: () => ({ title: casual.title, text: casual.sentences(3) })
 };
 
 export default mocks;
